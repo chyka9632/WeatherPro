8d8d8619b457fcae00ee434206082284905ea9fe
Chika <chyka9632@gmail.com>
2023-06-05 15:44:34 +0100

    Plot improvised data dynamically - using function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.express as px
 
-
 st.title("Weather Forecast for the Next Days")
 location = st.text_input("Location: ")
 days = st.slider("Forecast Days: ", min_value=1, max_value=5,
@@ -10,9 +9,14 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {location}")
 
 
-dates = ["2023-05-06", "2023-06-06", "2023-07-06", "2023-08-06", "2023-09-06"]
-temps = [14, 17, 13, 15, 19]
-temps = [days * i for i in temps]
+def get_data(days):
+    dates = ["2023-05-06", "2023-06-06", "2023-07-06", "2023-08-06", "2023-09-06"]
+    temps = [14, 17, 13, 15, 19]
+    temps = [days * i for i in temps]
+    return dates, temps
 
-figure = px.line(x=dates, y=temps, labels={"x": "Dates", "y": "Temperatures (C)"})
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperatures (C)"})
 st.plotly_chart(figure)
