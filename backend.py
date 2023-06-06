a34ac59571b2080757eef4f354f814013c124d79
Chika <chyka9632@gmail.com>
2023-06-06 21:11:18 +0100

    Filter the forecast data

--- backend.py
+++ backend.py
@@ -3,12 +3,20 @@ import requests as rs
 api_key = "86a8a1b81ac6c8f15753c9704c7d2a04"
 
 
-def get_data(location, days=None, option=None):
+def get_data(location, forecast_days=None, weather_parameter=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={api_key}"
     response = rs.get(url)
     content_data = response.json()
-    return content_data
+    filtered_data = content_data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if weather_parameter == "Temperature":
+        filtered_data = [x["main"]["temp"] for x in filtered_data]  # x is just a variable name
+    if weather_parameter == "Sky":
+        filtered_data = [y["weather"][0]["main"] for y in filtered_data]
+
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(location="Tokyo"))
+    print(get_data(location="Tokyo", forecast_days=2, weather_parameter="Temperature"))
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 location = st.text_input("Location: ")
-days = st.slider("Forecast Days: ", min_value=1, max_value=5,
+forecast_days = st.slider("Forecast Days: ", min_value=1, max_value=5,
                  help="Select the number of forecast days: ")
-option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {location}")
+weather_parameter = st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(f"{weather_parameter} for the next {forecast_days} days in {location}")
 
 
-d, t = get_data(location, days, option)
+d, t = get_data(location, forecast_days, weather_parameter)
 
 figure = px.line(x=d, y=t, labels={"x": "Dates", "y": "Temperatures (C)"})
 st.plotly_chart(figure)
